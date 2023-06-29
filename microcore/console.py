ed16e3abd292961835ab561a01f3e8f1dd3f2da9
Nayjest <mail@vitaliy.in>
2023-06-29 19:40:24 +0200

    ui.error added, console & decorators removed

--- microcore/console.py
+++
@@ -1,6 +0,0 @@
-import os
-from colorama import Fore, init
-init(autoreset=True)
-def print_sys(msg): print(Fore.LIGHTYELLOW_EX + msg)
-def debug_log(msg): print(Fore.BLUE + msg)
-def user_input(): return input().strip()
--- microcore/ui.py
+++ microcore/ui.py
@@ -5,5 +5,6 @@ _input = input
 
 def info(*args, color=Fore.LIGHTYELLOW_EX, **kwargs): print(*[color + i for i in args], **kwargs)
 def debug(msg): info(msg, color=Fore.BLUE)
+def error(*args, **kwargs): print(*[Fore.RED + i for i in args], **kwargs)
 def input(*args, **kwargs): return _input(*args, **kwargs).strip()
 def ask_yn(msg): return 'y' in input(msg + ' (y/n)').lower()
