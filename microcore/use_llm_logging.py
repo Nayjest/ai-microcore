7fc370b177f6189b318269df36ad222458909702
Nayjest <mail@vitaliy.in>
2023-07-01 12:52:51 +0200

    improve logging

--- microcore/use_llm_logging.py
+++ microcore/use_llm_logging.py
@@ -1,14 +1,26 @@
 import microcore
 import microcore.ui
-from colorama import Fore
+from colorama import Fore, Style
 
 _orig_llm = microcore.llm
 
 
+class logging_options:
+    prompt_color = Fore.LIGHTGREEN_EX
+    response_color = Fore.CYAN
+    indent = '\t'
+
+
 def logged_llm(prompt: str, **kwargs):
-    print(f'Requesting LLM:\n{Fore.LIGHTGREEN_EX}{prompt}')
+    nl = '\n' + logging_options.indent
+    prompt_indented = nl + nl.join(prompt.split('\n'))
+    print(
+        f'Requesting LLM {Fore.MAGENTA}{microcore.llm_default_args["model"]}{Style.RESET_ALL}:'
+        f'{logging_options.prompt_color}{prompt_indented}'
+    )
     out = _orig_llm(prompt, **kwargs)
-    print(f'LLM Answer: {Fore.CYAN}{out}')
+    out_indented = nl + nl.join(out.split('\n'))
+    print(f'LLM Response:{logging_options.response_color}{out_indented}')
     return out
 
 
