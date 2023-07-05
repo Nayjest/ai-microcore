7b22511c6b64f3b9266abd89164977e662d4cd4c
Nayjest <mail@vitaliy.in>
2023-07-05 19:03:15 +0200

    request chaining

--- microcore/use.py
+++ microcore/use.py
@@ -1,4 +1,5 @@
 from microcore import *
 
+# @todo Dont works if you do 'from microcore import *'
 def logging(): import microcore.logging
 def model(name: str): microcore.llm_default_args['model'] = name
