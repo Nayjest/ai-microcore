67cce00ac0f86c60bc941c22782e298e07e46a12
Nayjest <mail@vitaliy.in>
2023-11-11 19:07:42 +0100

    ui.ask_choose implemented, ui module added to main package imports to be accessible via mc.ui

--- microcore/ui.py
+++ microcore/ui.py
@@ -18,3 +18,24 @@ def error(*args, **kwargs):
 
 def ask_yn(msg):
     return "y" in input(msg + " (y/n)").lower().strip()
+
+
+def ask_choose(msg, variants: list):
+        i = 0
+        if isinstance(variants, list):
+            for item in variants:
+                i += 1
+                print(f"\t{Fore.MAGENTA}{i}:{Fore.RESET}\t{item}")
+        while True:
+            i = input(f"{msg} {Fore.MAGENTA}[1-{len(variants)}]{Fore.RESET}: ")
+            if not i.isdigit():
+                error("Not a number")
+                continue
+            i = int(i) - 1
+            if i >= len(variants) or i < 0:
+                error("Incorrect number")
+                continue
+            break
+
+        item = variants[int(i)]
+        return item
