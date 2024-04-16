e8a18db943e14e244d96d75c889cd42e3fb63343
Nayjest <mail@vitaliy.in>
2024-04-16 12:13:12 +0200

    Fix API tests (RuntimeError: Could not determine home directory from chromadb init -> pathlib -> os.environ['USERPROFILE'] is required)

--- tests/apis/setup_env.py
+++ tests/apis/setup_env.py
@@ -11,14 +11,14 @@ envs = glob.glob(".env.test.*")
 # envs = ['.env.test.anyscale']
 # envs = ['.env.test.open_ai-instruct']
 
-
 @pytest.fixture(params=envs)
 def setup_env(request):
     logging.info(f"\n{c.MAGENTA}===  [ SETUP ENV {request.param} ] ===")
     original_env = dict(os.environ)
-    os.environ.clear()
     microcore.configure(
-        DOT_ENV_FILE=request.param, LLM_DEFAULT_ARGS=dict(temperature=0.01)
+        USE_DOT_ENV=True,
+        DOT_ENV_FILE=request.param,
+        LLM_DEFAULT_ARGS=dict(temperature=0.01)
     )
     yield
     os.environ.clear()
