b666e647d9058e79a3bc6572038ee71d89aea9ed
Nayjest <mail@vitaliy.in>
2025-04-16 13:23:39 +0200

    Fix ChromaDB 1+ OverflowError: out of range integral type conversion attempted

--- microcore/embedding_db/__init__.py
+++ microcore/embedding_db/__init__.py
@@ -8,6 +8,8 @@ import tiktoken
 from ..utils import ExtendedString
 
 
+INT32_MAX = 2**31 - 1  # 2147483647
+
 class SearchResults(list):
     def fit_to_token_size(
         self,
@@ -115,7 +117,7 @@ class AbstractEmbeddingDB(ABC):
         **kwargs,
     ) -> SearchResults | list[str | SearchResult]:
         return self.search(
-            collection, query, n_results=sys.maxsize - 1, where=where, **kwargs
+            collection, query, n_results=INT32_MAX, where=where, **kwargs
         )
 
     @abstractmethod
