216fcadd7eba2e2c94ea6ab66692afc85117c18d
Nayjest <mail@vitaliy.in>
2025-04-16 13:50:13 +0200

    Fix tests for python v3.11 and older

--- tests/basic/test_role_enum.py
+++ tests/basic/test_role_enum.py
@@ -1,3 +1,7 @@
+import sys
+
+import pytest
+
 from microcore.message_types import Role
 
 
@@ -9,12 +13,21 @@ def test_role():
     assert (Role.USER + 'str') == "userstr"
     assert ','.join(Role) == "system,user,assistant"
 
+
 def test___str__():
     assert str(Role.SYSTEM) == "system"  # __str__
     assert str(Role.ASSISTANT) == "assistant"
     assert f"{Role.USER}" == "user"
     assert "%s" % Role.USER == "user"
 
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 12),
+    reason="""
+    in Python 3.11.X and older __contains__ will raise TypeError:
+    unsupported operand type(s) for 'in': 'str' and 'EnumMeta'
+    """
+)
 def test_in():
     assert Role.USER in Role
     assert "user" in Role
