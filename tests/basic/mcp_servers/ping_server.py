84a4ff7b89841ed1641efeff488ccc7e29132edd
Nayjest <mail@vitaliy.in>
2025-06-09 02:51:23 +0200

    minor fix

--- tests/basic/mcp_servers/ping_server.py
+++ tests/basic/mcp_servers/ping_server.py
@@ -4,7 +4,7 @@ from mcp.server.fastmcp import FastMCP
 
 parser = argparse.ArgumentParser(description='MCP Server')
 parser.add_argument('--port', type=int, default=8881)
-parser.add_argument('--transport', type=str, default='sse', help='sse|stdio|streamable-http)')
+parser.add_argument('--transport', type=str, default='sse', help='sse|stdio|streamable-http')
 args = parser.parse_args()
 
 mcp = FastMCP(debug=True, host='127.0.0.1', port=args.port)
