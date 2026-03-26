fd8ca6c37624395f77ac5e0c9b69d550483c5fd1
Nayjest <mail@vitaliy.in>
2026-03-26 18:34:48 +0100

    additional PromptWrapper tests

--- tests/basic/test_wrappers.py
+++ tests/basic/test_wrappers.py
@@ -1,7 +1,8 @@
 import microcore as mc
-from . import setup
-
+from microcore.message_types import Role
 from microcore.wrappers.prompt_wrapper import PromptWrapper
+
+from . import setup
 
 
 async def test_wrappers(setup):
@@ -11,13 +12,11 @@ async def test_wrappers(setup):
     out = (await mc.tpl(file="json_data.j2", var="test_data").to_allm()).parse_json()
     assert out == dict(data="test_data")
 
-
 def test_creation_basic():
     p = PromptWrapper("Hello world")
     assert p == "Hello world"
     assert p.tpl_file is None
     assert p.tpl_vars is None
-
 
 def test_creation_with_attrs():
     p = PromptWrapper("test", attrs={"key": "value"})
@@ -29,6 +28,13 @@ def test_creation_with_tpl_file():
     assert p.tpl_file == "template.txt"
     assert p.tpl_vars == {"a": 1}
 
+def test_as_message():
+    p = PromptWrapper("Hello world")
+    assert isinstance(p.as_user, mc.UserMsg)
+    assert isinstance(p.as_system, mc.SysMsg)
+    assert isinstance(p.as_assistant, mc.AssistantMsg)
+    assert p.as_user.content == "Hello world"
+    assert p.as_user.role == Role.USER
 
 def test_is_instance_of_str():
     p = PromptWrapper("hello")
